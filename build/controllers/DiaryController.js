9893fc57028052cc7b5b71836e53c0a1642e5f41
Nwashangai <nwashangai@gmail.com>
2018-07-19 20:20:04 +0100

    fetch all diaries

--- build/controllers/DiaryController.js
+++ build/controllers/DiaryController.js
@@ -7,9 +7,5 @@ var _DiaryModel2 = _interopRequireDefault(_DiaryModel);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.getDiary = function (request, response) {
-  if (request.params.id) {
-    response.status(200).json(_DiaryModel2.default.data[request.params.id]);
-  } else {
-    response.status(200).json(_DiaryModel2.default.data);
-  }
+  response.status(200).json(_DiaryModel2.default.data);
 };
--- src/controllers/DiaryController.js
+++ src/controllers/DiaryController.js
@@ -1,8 +1,4 @@
 import DiaryModel from '../models/DiaryModel';
 exports.getDiary = (request, response) => {
-  if (request.params.id) {
-    response.status(200).json(DiaryModel.data[request.params.id]);
-  } else {
-    response.status(200).json(DiaryModel.data);
-  }
+  response.status(200).json(DiaryModel.data);
 }
