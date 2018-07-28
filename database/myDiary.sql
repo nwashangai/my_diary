bde0a0ac4cc7a47538a9c9a03663b8096d013848
Nwashangai <nwashangai@gmail.com>
2018-07-28 19:16:44 +0100

    user sign up and login

--- database/myDiary.sql
+++ database/myDiary.sql
@@ -6,7 +6,7 @@
 -- Dumped by pg_dump version 9.5.13
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
+-- SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
