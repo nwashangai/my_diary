69c12be7eb3c3d68f603cb7459ce9bc850ec7554
Nwashangai <nwashangai@gmail.com>
2018-07-28 10:16:06 +0100

    user sign up and login

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 module.exports = {
 
   PORT: process.env.PORT,
-  DATABASE_URL: 'postgres://postgres:andela@localhost:5432/myDiary',
+  DATABASE_URL: 'postgres://andela:andela@127.0.0.1:5432/myDiary',
   SECRET: 'asdfghjkl',
   POOL: {
-    user: 'postgres', // env var: PGUSER
+    user: 'andela', // env var: PGUSER
     database: 'myDiary', // env var: PGDATABASE
     password: 'andela', // env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
+    host: '127.0.0.1', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
     max: 10, // max number of clients in the pool
   }
