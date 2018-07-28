4f7bd74e58346e113e12bd2d6c79af829495ca00
Nwashangai <nwashangai@gmail.com>
2018-07-28 18:18:49 +0100

    user sign up and login

--- build/config.js
+++ build/config.js
@@ -8,7 +8,7 @@ module.exports = {
   POOL: {
     user: 'postgres', // env var: PGUSER
     database: 'myDiary', // env var: PGDATABASE
-    password: 'andela', // env var: PGPASSWORD
+    password: '1234', // env var: PGPASSWORD
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
     max: 10 // max number of clients in the pool
